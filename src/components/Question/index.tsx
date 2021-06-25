94f39731c43b113badbd25244c5e2674776494a9
igorWebMASTER <igor_1917@hotmail.com>
2021-06-24 21:54:45 -0300

    feat: add question component

---
+++ src/components/Question/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ReactNode } from 'react'
+
+import './styles.scss'
+
+type QuestionProps = {
+    content: string;
+    author: {
+        name: string;
+        avatar: string;
+    }
+    children?: ReactNode;
+
+}
+
+export function Question({ content, children, author }: QuestionProps) {
+    return (
+        <div className='question'>
+            <p>{content}</p>
+            <footer>
+                <div className="user-info">
+                    <img src={author.avatar} alt={author.name} />
+                    <span>{author.name}</span>
+                </div>
+                <div>
+                    {children}
+                </div>
+            </footer>
+        </div>
+    )
+}
