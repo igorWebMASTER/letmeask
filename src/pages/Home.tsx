5009f3c97ae87f8de9b4c4ad50cb09aff852153c
igorWebMASTER <igor_1917@hotmail.com>
2021-06-24 22:03:29 -0300

    feat: verify if a room its closed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,33 +15,38 @@ import { useAuth } from '../hooks/useAuth'
 import { FormEvent, useState } from 'react'
 import { database } from '../services/firebase'
 
-export function Home(){
+export function Home() {
     const history = useHistory()
-    const { signInWithGoogle, user }  = useAuth()
-    const [ roomCode, setRoomCode] = useState('')
-    
+    const { signInWithGoogle, user } = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
-    async function handleCreateRoom(){
-        if(!user){
-           await signInWithGoogle()
+
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
         }
-       
+
         history.push('/rooms/new')
     }
 
 
-    async function handleJoinRoom(e: FormEvent){
+    async function handleJoinRoom(e: FormEvent) {
         e.preventDefault();
 
-        if(roomCode.trim() === ''){
+        if (roomCode.trim() === '') {
             return;
         }
 
         const roomRef = await database.ref(`rooms/${roomCode}`).get();
         console.log(roomRef)
 
-        if(!roomRef.exists()){
+        if (!roomRef.exists()) {
             alert('Rooms doesn not exists')
+            return;
+        }
+
+        if (roomRef.val().endedAt) {
+            alert('Sala já fechada.')
             return;
         }
 
@@ -50,14 +55,14 @@ export function Home(){
     }
 
     return (
-       <div id="page-auth">
-           <aside>
-               <img src={IlustrationImg} alt="image" />
-               <strong>Crie salas de Q&amp;A ao-vivo</strong>
-               <p>Tire suas dúvidas da sua audiência em tempo-real</p>
-           </aside>
-           <main>
-               <div className="main-content">
+        <div id="page-auth">
+            <aside>
+                <img src={IlustrationImg} alt="image" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire suas dúvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIcon} alt="Logo do google" />
@@ -67,18 +72,18 @@ export function Home(){
                         ou entre em uma sala
                     </div>
                     <form onSubmit={handleJoinRoom}>
-                        <input 
-                            type="text" name="" 
-                            placeholder="Digite uma sala" 
+                        <input
+                            type="text" name=""
+                            placeholder="Digite uma sala"
                             id=""
                             onChange={e => setRoomCode(e.target.value)}
-                         />
+                        />
                         <Button type="submit">
-                                Entrar na sala 
+                            Entrar na sala
                         </Button>
                     </form>
-               </div>
-           </main>
-       </div> 
+                </div>
+            </main>
+        </div>
     )
 }
