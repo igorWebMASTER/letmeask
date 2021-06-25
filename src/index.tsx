b2223ecd406d5e7ba2eeda5c2c745d0788ec3698
igorWebMASTER <igor_1917@hotmail.com>
2021-06-24 21:53:31 -0300

    add style to hot toast

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import  { Toaster } from 'react-hot-toast';
+
 
 import './services/firebase'
 
@@ -9,6 +11,23 @@ import './styles/global.scss'
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Toaster toastOptions={{
+    // Define default options
+    className: '',
+    duration: 5000,
+    style: {
+      background: '#e2e2e2',
+      color: '#1d1d1d',
+    },
+    // Default options for specific types
+    success: {
+      duration: 3000,
+      theme: {
+        primary: 'green',
+        secondary: 'black',
+      },
+    },
+  }} />
   </React.StrictMode>,
   document.getElementById('root')
 );
